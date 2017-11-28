a0a1ff379c04cc2c73d8f5cc5e20b7ec1bbd8ef4
mark <kingmaidol@163.com>
2017-11-29 00:09:15 +0800

    调整test配置

--- config/config.test.js
+++ config/config.test.js
@@ -1,31 +1,61 @@
 module.exports = app => ({
-	robot: {
-		ua: [
-			/sina/i,
-			/Baiduspider/i
-		]
-	},
-	cwLogger: {
-		logLevel4console: 'info', // console
-		bunyan: {
-			// 级别分别是: TRACE DEBUG INFO WARN ERROR FATAL
-			categorys: {
-				console: {
-					logLevel4console: 'debug',
-					// pretty: true
-				}
-			}
-		}
-	},
-	agentLogger: {
-		logLevel4console: 'info', // console
-		bunyan: {
-			// 级别分别是: TRACE DEBUG INFO WARN ERROR FATAL
-			categorys: {
-				console: {
-					logLevel4console: 'debug',
-				}
-			}
-		}
-	}
+  robot: {
+    ua: [
+      /sina/i,
+      /Baiduspider/i
+    ]
+  },
+  cwLogger: {
+    logLevel4console: 'info', // console
+    bunyan: {
+      // 级别分别是: TRACE DEBUG INFO WARN ERROR FATAL
+      categorys: {
+        console: {
+          logLevel4console: 'debug',
+          // pretty: true
+        }
+      }
+    },
+    enableLogstash4console: true, // 使用elk收集日志, 依赖enableBunyan
+    currentLogstashInput: 'tcp', // tcp数据包(相比udp, 大小默认无限制)
+    logstash: {
+      type: 'mysample' + (process.env.SITE_DOMAIN ? `-${process.env.SITE_DOMAIN}` : ''),
+      udp: {
+        host: '192.168.2.155',
+        port: 64100,
+        type: 'mysample' + (process.env.SITE_DOMAIN ? `-${process.env.SITE_DOMAIN}` : '')
+      },
+      tcp: {
+        host: '192.168.2.155',
+        port: 64756,
+        type: 'mysample' + (process.env.SITE_DOMAIN ? `-${process.env.SITE_DOMAIN}` : '')
+      }
+    },
+  },
+  agentLogger: {
+    logLevel4console: 'info', // console
+    bunyan: {
+      // 级别分别是: TRACE DEBUG INFO WARN ERROR FATAL
+      categorys: {
+        console: {
+          logLevel4console: 'debug',
+        }
+      }
+    },
+    enableLogstash4console: true, // 使用elk收集日志, 依赖enableBunyan
+    currentLogstashInput: 'tcp', // tcp数据包(相比udp, 大小默认无限制)
+    logstash: {
+      type: 'mysample' + (process.env.SITE_DOMAIN ? `-${process.env.SITE_DOMAIN}` : ''),
+      udp: {
+        host: '192.168.2.155',
+        port: 64100,
+        type: 'mysample' + (process.env.SITE_DOMAIN ? `-${process.env.SITE_DOMAIN}` : '')
+      },
+      tcp: {
+        host: '192.168.2.155',
+        port: 64756,
+        type: 'mysample' + (process.env.SITE_DOMAIN ? `-${process.env.SITE_DOMAIN}` : '')
+      }
+    },
+  }
 });
