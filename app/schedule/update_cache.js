60eab633fb6a0f0daa77b179e3b8b17ce1fbef57
mark <kingmaidol@163.com>
2017-11-28 22:36:40 +0800

    update

--- app/schedule/update_cache.js
+++ app/schedule/update_cache.js
@@ -8,7 +8,7 @@ module.exports = class UpdateCache extends Subscription {
   // 通过 schedule 属性来设置定时任务的执行间隔等配置
   static get schedule() {
     return {
-      interval: '1s', // 1 分钟间隔
+      interval: '1m', // 1 分钟间隔
       type: 'all', // 指定所有的 worker 都需要执行
     };
   }
